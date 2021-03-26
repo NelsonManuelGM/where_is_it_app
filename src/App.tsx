3751529f7b31d955db40a51817ca31a487c60816
Nelson <nelsitogm25@gmail.com>
2021-03-26 16:30:54 -0400

    updating App

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
 import {useEffect, useState} from 'react';
 import {useGetToken} from './services';
-import MapComponent from './components/Map';
-import {places as listPlaces} from './services/places';
-import {Place} from "./components/Map/interfaces";
-import {LatLngLiteral} from "leaflet";
-let date = new Date()
-
+import Dashboard from "./components/Dashboard";
 
 function App() {
     const [token, setToken] = useState<string>('')
 
-    const [places,setPlaces] = useState<Array<Place>>(listPlaces)
-
-    const [target,setTarget] = useState<Array<LatLngLiteral>>([{lat: 25.684209407950874, lng: -80.41917278495983}])
-
-    const [zoom, setZoom] = useState(14);
-
-    useEffect(()=>{
+    useEffect(() => {
         setToken(useGetToken)
-    },[])
+    }, [])
 
-  return (
-    <div className="App">
-      <header className="App-header">
-         {/*<button onClick={()=>alert(token)}>Click</button>*/}
-
-         <MapComponent places={places} target={target} zoom={zoom}/>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/*<button onClick={()=>alert(token)}>Click</button>*/}
+                <Dashboard/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
