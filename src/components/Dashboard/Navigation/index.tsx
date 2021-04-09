5e13662cdf12ccf316ff164282ac90427782f1ed
Nelson <nelsitogm25@gmail.com>
2021-04-09 19:30:29 -0400

    updating Navigation Component

--- src/components/Dashboard/Navigation/index.tsx
+++ src/components/Dashboard/Navigation/index.tsx
@@ -5,10 +5,11 @@ import NavigationDrawer from "./NavigationDrawer";
 import {NavigationComponentProps} from "./interfaces";
 import {NavigationManeuver} from "./NavigationBox/interfaces";
 import distanceBetweenPoint from "../../../services/distanceBetweenPoint";
+import LocomotionPicker from "./LocomotioPicker";
 
-const NavigationComponent:FC<NavigationComponentProps> = ({direction, location}) => {
+const NavigationComponent: FC<NavigationComponentProps> = ({direction, location, responsive, onClickLocomotion}) => {
     const [currentStep, setCurrentStep] = useState<number>()
-    
+
     const [navigation, setNavigation] = useState<NavigationManeuver>();
 
     const [drawerFlag, setDrawerFlag] = useState<boolean>(false)
@@ -17,37 +18,41 @@ const NavigationComponent:FC<NavigationComponentProps> = ({direction, location})
         setDrawerFlag(!drawerFlag)
     }, [drawerFlag])
 
-    useEffect(()=>{
-        //TODO fix distance
-        direction?.routes[0].legs[0].steps.forEach((step,index)=>{
+    useEffect(() => {
+        direction?.routes[0].legs[0].steps.forEach((step, index) => {
             let distance = 0
-            step.geometry.coordinates.forEach((coordinate)=>{
+            step.geometry.coordinates.forEach((coordinate) => {
                 distance = distanceBetweenPoint(location.lat, location.lng, coordinate[0], coordinate[1])
-                console.log('DISTANCE IN METERS INSIDE LOOP: ',parseInt(distance.toFixed(2)))
-                if(parseInt(distance.toFixed(2)) < 30)
+                //TODO delete this
+                console.log('DISTANCE -> METERS -> : ', parseInt(distance.toFixed(2)))
+
+                //TODO customizable error for current position
+                if (parseInt(distance.toFixed(2)) < 100)
                     setCurrentStep(index)
             })
 
         })
-    },[direction?.routes, location.lat, location.lng])
+    }, [direction?.routes, location.lat, location.lng])
 
-    useEffect(()=>{
+    useEffect(() => {
         if (currentStep !== undefined) {
             setNavigation({
-                type: direction?.routes[0].legs[0].steps[currentStep].maneuver.type!, //'departure',
-                modifier: direction?.routes[0].legs[0].steps[currentStep].maneuver.modifier,//'straight',
+                type: direction?.routes[0].legs[0].steps[currentStep].maneuver.type!,
+                modifier: direction?.routes[0].legs[0].steps[currentStep].maneuver.modifier,
                 instruction: direction?.routes[0].legs[0].steps[currentStep].maneuver.instruction,
                 name: direction?.routes[0].legs[0].steps[currentStep].name,
                 destination: direction?.routes[0].legs[0].steps[currentStep].destinations,
             })
         }
-    },[currentStep, direction?.routes])
+    }, [currentStep, direction?.routes])
 
     return <>
+        <NavigationBox navigation={navigation} onClickBox={onClickBox} responsive={responsive}/>
         {
-            navigation && <NavigationBox navigation={navigation} onClickBox={onClickBox}/>
+            navigation && <LocomotionPicker onClickLocomotion={onClickLocomotion}/>
         }
-        <NavigationDrawer open={drawerFlag} onDrawerClose={onClickBox} route={direction?.routes[0]}/>
+        <NavigationDrawer open={drawerFlag && currentStep !== undefined} onDrawerClose={onClickBox}
+                          route={direction?.routes[0]} currentStep={currentStep}/>
     </>
 }
 
--- src/components/Dashboard/Navigation/interfaces.ts
+++ src/components/Dashboard/Navigation/interfaces.ts
@@ -4,5 +4,8 @@ import {Direction} from "../../../services/mapbox/interfaces";
 
 export interface NavigationComponentProps {
     direction?: Direction,
-    location: LatLngLiteral
+    location: LatLngLiteral,
+    responsive: boolean,
+    onClickLocomotion : (value:string) => void
+
 }
