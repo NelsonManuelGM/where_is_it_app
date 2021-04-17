21316e9348e4595590e2465679c5fdf710859366
Nelson <nelsitogm25@gmail.com>
2021-04-17 15:17:14 -0400

    updating NavigationBox style

--- src/components/Dashboard/Navigation/NavigationBox/index.tsx
+++ src/components/Dashboard/Navigation/NavigationBox/index.tsx
@@ -1,26 +1,26 @@
-import React, { FC, memo } from "react";
-import { ArrowBackIos } from "@material-ui/icons";
-import { CircularProgress, ListItemText, Typography, useTheme } from "@material-ui/core";
+import React, {FC, memo} from "react";
+import {ArrowBackIos} from "@material-ui/icons";
 import styled from "styled-components";
+import {CircularProgress, Typography, useTheme} from "@material-ui/core";
 
-import { customStyles } from "../../../../context/theme";
-import { NavigatorProps } from "./interfaces";
+import {customStyles} from "../../../../context/theme";
+import {NavigatorProps} from "./interfaces";
 import SignalGenerator from "../NavigationDrawer/SignalGenerator";
-import { Step } from "../../../../services/mapbox/interfaces";
+import {Step} from "../../../../services/mapbox/interfaces";
 
-const Cover = styled.div` 
+const Cover = styled.div`
   min-height: 40px;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
   cursor: pointer;
-  margin-bottom:5px;
+  margin-bottom: 5px;
 `
 
-const NavigationBox: FC<NavigatorProps> = ({ navigation, onClickBox, responsive }) => {
+const NavigationBox: FC<NavigatorProps> = ({navigation, onClickBox, responsive}) => {
     const theme = useTheme()
-    const { navigationIcon, dashboardStyle } = customStyles()
+    const {navigationIcon, dashboardStyle} = customStyles()
 
 
     // TODO separate this function and the one in the drawer to an external file
@@ -36,39 +36,38 @@ const NavigationBox: FC<NavigatorProps> = ({ navigation, onClickBox, responsive 
     }
 
     return <Cover theme={theme} onClick={onClickBox} className={dashboardStyle}
-        style={{ width: responsive ? '80px' : '260px' }}>
+                  style={{width: responsive ? '80px' : '260px'}}>
         {
             navigation ? <div style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-                width: '100%'
-            }}>
-                <ArrowBackIos fontSize={'small'} style={{ margin: '0 0.5rem' }} />
-                {
-                    !responsive && <div style={{ display: 'flex', flexDirection: 'column' }}>
-                        <ListItemText primary={navigation.maneuver.instruction} secondary={
-                            <>
-                                <Typography component='span' variant='body2' style={{ color: theme.palette.secondary.main }}>
-                                    {
-                                        handleStepString(navigation)
-                                    }
-                                </Typography>
-                                <p style={{ margin: '1px' }}>   {navigation.distance.toFixed()} meters  | {(navigation.duration / 60).toFixed()} min</p>
-                            </>
-                        } />
-                    </div>
-                }
-                {
-                    SignalGenerator({
-                        type: navigation.maneuver.type,
-                        modifier: navigation.maneuver.modifier,
-                        iconCssClass: navigationIcon
-                    })
-                }
-            </div>
-                : <CircularProgress disableShrink style={{ color: "white" }} size={25} />
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    width: '100%'
+                }}>
+                    <ArrowBackIos fontSize={'small'} style={{margin: '0 0.5rem'}}/>
+                    {
+                        !responsive && <div style={{display: 'flex', flexDirection: 'column'}}>
+                            <Typography variant={'body1'}>{navigation.maneuver.instruction}</Typography>
+
+                            <Typography variant='body2' style={{color: theme.palette.secondary.main}}>{
+                                handleStepString(navigation)
+                            }</Typography>
+
+                            <Typography variant='subtitle2' style={{color: theme.palette.grayscale.main}}>
+                                {navigation.distance.toFixed()} meters | {(navigation.duration / 60).toFixed()} min
+                            </Typography>
+                        </div>
+                    }
+                    {
+                        SignalGenerator({
+                            type: navigation.maneuver.type,
+                            modifier: navigation.maneuver.modifier,
+                            iconCssClass: navigationIcon
+                        })
+                    }
+                </div>
+                : <CircularProgress disableShrink style={{color: "white"}} size={25}/>
         }
     </Cover>
 }
