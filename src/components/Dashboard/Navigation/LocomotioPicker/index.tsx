e8fcd8bcc6c41854f6d40698138d0bd718f56c6d
Nelson <nelsitogm25@gmail.com>
2021-04-17 12:01:36 -0400

    Updating locomotioPicker Component

--- src/components/Dashboard/Navigation/LocomotioPicker/index.tsx
+++ src/components/Dashboard/Navigation/LocomotioPicker/index.tsx
@@ -1,36 +1,29 @@
 import {FC, memo, useState} from "react";
-import {BottomNavigation, BottomNavigationAction, useTheme} from "@material-ui/core";
+import {BottomNavigation, BottomNavigationAction} from "@material-ui/core";
 import {DirectionsBikeOutlined, DirectionsWalkOutlined, DriveEtaOutlined} from "@material-ui/icons";
 
-import styled from "styled-components";
 import {customStyles} from "../../../../context/theme";
 import {LocomotionPickerProps} from "./interfaces";
 import {Profile} from "../../../../services/mapbox/interfaces";
 
-const Cover = styled.div`
-  top: 80px;
-  right: 25px;
-  padding: 0;
-`
 
 const LocomotionPicker: FC<LocomotionPickerProps> = ({onClickLocomotion}) => {
-    const theme = useTheme()
     const {dashboardStyle} = customStyles()
 
     const [value, setValue] = useState(0)
 
-    return <Cover theme={theme} className={dashboardStyle}>
+    return <div style={{padding:0, width:'115px'}} className={dashboardStyle}>
         <BottomNavigation value={value} onChange={(event, newValue) => {
             setValue(newValue);
         }}>
             <BottomNavigationAction onClick={() => onClickLocomotion(Profile.driving)}
-                                    icon={<DriveEtaOutlined fontSize={"small"}/>}/>
+                                    icon={<DriveEtaOutlined fontSize={"small"}/>} about={"Driving"}/>
             <BottomNavigationAction onClick={() => onClickLocomotion(Profile.cycling)}
-                                    icon={<DirectionsBikeOutlined fontSize={"small"}/>}/>
+                                    icon={<DirectionsBikeOutlined fontSize={"small"}/>} about={"Cycling"}/>
             <BottomNavigationAction onClick={() => onClickLocomotion(Profile.walking)}
-                                    icon={<DirectionsWalkOutlined fontSize={"small"}/>}/>
+                                    icon={<DirectionsWalkOutlined fontSize={"small"}/>} about={"Walking"}/>
         </BottomNavigation>
-    </Cover>
+    </div>
 }
 
 export default memo(LocomotionPicker)
