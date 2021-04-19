cd44130e1662e109aa2fcac905d6339b8c1f3d2a
Nelson <nelsitogm25@gmail.com>
2021-04-19 10:45:40 -0400

    updating appStyle

--- src/context/appStyle.tsx
+++ src/context/appStyle.tsx
@@ -3,35 +3,12 @@ import {theme} from './theme'
 
 const AppStyle = createGlobalStyle`
   .leaflet-div-icon {
-    background:transparent;
+    background: transparent;
     border: none;
   }
 
-  .MuiDrawer-paper{
-    background: ${theme.palette.grayscale.dark};
-    color: ${theme.palette.grayscale.light};
-    opacity: 90%;
-    width: 20rem;
-  }
-  .MuiTypography-colorTextSecondary {
-    color: ${theme.palette.grayscale.main};
-  }
   .MuiBottomNavigation-root{
-    height: unset;
-    background-color: unset;
-    min-width:unset;
-  }
-  .MuiBottomNavigationAction-root.Mui-selected{
-    padding: 8px;
-    color: ${theme.palette.warning.main};
-  }
-  .MuiBottomNavigationAction-root.MuiBottomNavigationAction-iconOnly{
-    padding: 8px;
-  }
-  .MuiBottomNavigationAction-root{
-    padding: 8px;
-    color: ${theme.palette.grayscale.light};
-    min-width:unset;
+    background-color: ${theme.palette.grayscale.dark};
   }
 `;
 
